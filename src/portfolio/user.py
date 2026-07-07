220ca711ed33cd7fa5fb36da20143e0546d9aaa6
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-07-07 21:26:01 +0200

    feat: simulate do-while loop for insert user data in the project.

--- src/portfolio/user.py
+++ src/portfolio/user.py
@@ -12,8 +12,20 @@ def presentation_menu():
 def user_initialization():
     presentation_menu()
     
-    name = input("What's your name?\n=> ")
-    budget = float(input("What's your budget for this month?\n=> "))
-    starting_currency = float(input("What's your initial balance?\n=> "))
-
+    while True:
+        name = input("What's your name?\n=> ")
+        budget = input("What's your budget for this month?\n=> ")
+        starting_currency = input("What's your initial balance?\n=> ")
+        
+        error = validated_user(name, budget, starting_currency)
+        
+        if error and error.get("error"):
+            print(error.get("message"))
+        else:
+            return {
+                "name": name, 
+                "budget": float(budget), 
+                "starting_currency": float(starting_currency)
+            }
     
+print(user_initialization())
